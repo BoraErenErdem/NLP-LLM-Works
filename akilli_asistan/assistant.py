5fd3e2db39b3598185756a614f9c9d4e8f566696
Bora Eren Erdem <erdemeren3460@gmail.com>
2026-06-22 17:26:51 +0300

    The detect intent function was used to create an intent classifier for the message received from the user. A summary classifier was also added for special days.

--- akilli_asistan/assistant.py
+++ akilli_asistan/assistant.py
@@ -5,46 +5,31 @@ from dotenv import load_dotenv # .env dosyasından api key'i yüklemek
 import requests # http istekleri yönetebilmek için
 
 load_dotenv(r'C:\Projects\google_codes\akilli_asistan\.env')
-#api_key = os.getenv('GEMINI_API_KEY')
-api_key = os.getenv("GROQ_API_KEY")
+api_key = os.getenv('GEMINI_API_KEY')
 
 if not api_key:
     raise ValueError('Gemini API key not found!')
 
 # gemini 2.5 flash modelinin api url'i
-#url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent" # gemini url adresi
+url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent" # gemini url adresi
 
-url = "https://api.groq.com/openai/v1/chat/completions"
-
-# # api çağrısı için gerekli http başlıkları # headers -> kimlik kartı
-# headers = {
-#     "Content-Type": "application/json", # json formatında veri gönderileceğini belirtir
-#     "X-Goog-Api-Key": api_key # yetkilendirme için tanımlanan api_key
-# }
-
+# api çağrısı için gerekli http başlıkları # headers -> kimlik kartı
 headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
+    "Content-Type": "application/json", # json formatında veri gönderileceğini belirtir
+    "X-Goog-Api-Key": api_key # yetkilendirme için tanımlanan api_key
 }
 
 # gemini api'sine prompt gönderip yanıt alan fonksiyon
 def get_gemini_response(prompt:str):
 
     # api'ye gönderilecek olan json yapısı
-    # payload = {
-    #     "contents": [
-    #         {
-    #             "parts": [
-    #                 {"text": prompt} # kullanıcıdan gelen mesajı içeren bölüm
-    #             ]
-    #         }
-    #     ]
-    # }
-
     payload = {
-        "model": "llama-3.3-70b-versatile",
-        "messages": [
-            {"role": "user", "content": prompt}
+        "contents": [
+            {
+                "parts": [
+                    {"text": prompt} # kullanıcıdan gelen mesajı içeren bölüm
+                ]
+            }
         ]
     }
 
@@ -54,8 +39,7 @@ def get_gemini_response(prompt:str):
     if response.status_code == 200: # istek başarılıysa (http 200)
         try:
             result = response.json() # json formatındaki yanıtı dictionary'e çevir
-            #return result["candidates"][0]["content"]["parts"][0]["text"] # payload veri paketindeki verileri teker teker açarak text kısmındaki cevaba erişiyorum..!
-            return result["choices"][0]["message"]["content"]
+            return result["candidates"][0]["content"]["parts"][0]["text"] # payload veri paketindeki verileri teker teker açarak text kısmındaki cevaba erişiyorum..!
         except Exception as e:
             return f"Yanıt hatasi {e}"
     else: # istek başarısızsa (hata varsa)
