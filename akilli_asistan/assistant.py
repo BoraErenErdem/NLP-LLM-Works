267d7d093db465a357e1af65ac9b2b87da7f6e70
Bora Eren Erdem <erdemeren3460@gmail.com>
2026-06-22 17:25:09 +0300

    add assistant.py detect_intent custom function

--- akilli_asistan/assistant.py
+++ akilli_asistan/assistant.py
@@ -5,31 +5,46 @@ from dotenv import load_dotenv # .env dosyasından api key'i yüklemek
 import requests # http istekleri yönetebilmek için
 
 load_dotenv(r'C:\Projects\google_codes\akilli_asistan\.env')
-api_key = os.getenv('GEMINI_API_KEY')
+#api_key = os.getenv('GEMINI_API_KEY')
+api_key = os.getenv("GROQ_API_KEY")
 
 if not api_key:
     raise ValueError('Gemini API key not found!')
 
 # gemini 2.5 flash modelinin api url'i
-url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent" # gemini url adresi
+#url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent" # gemini url adresi
 
-# api çağrısı için gerekli http başlıkları # headers -> kimlik kartı
+url = "https://api.groq.com/openai/v1/chat/completions"
+
+# # api çağrısı için gerekli http başlıkları # headers -> kimlik kartı
+# headers = {
+#     "Content-Type": "application/json", # json formatında veri gönderileceğini belirtir
+#     "X-Goog-Api-Key": api_key # yetkilendirme için tanımlanan api_key
+# }
+
 headers = {
-    "Content-Type": "application/json", # json formatında veri gönderileceğini belirtir
-    "X-Goog-Api-Key": api_key # yetkilendirme için tanımlanan api_key
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
 }
 
 # gemini api'sine prompt gönderip yanıt alan fonksiyon
 def get_gemini_response(prompt:str):
 
     # api'ye gönderilecek olan json yapısı
+    # payload = {
+    #     "contents": [
+    #         {
+    #             "parts": [
+    #                 {"text": prompt} # kullanıcıdan gelen mesajı içeren bölüm
+    #             ]
+    #         }
+    #     ]
+    # }
+
     payload = {
-        "contents": [
-            {
-                "parts": [
-                    {"text": prompt} # kullanıcıdan gelen mesajı içeren bölüm
-                ]
-            }
+        "model": "llama-3.3-70b-versatile",
+        "messages": [
+            {"role": "user", "content": prompt}
         ]
     }
 
@@ -39,14 +54,27 @@ def get_gemini_response(prompt:str):
     if response.status_code == 200: # istek başarılıysa (http 200)
         try:
             result = response.json() # json formatındaki yanıtı dictionary'e çevir
-            return result["candidates"][0]["content"]["parts"][0]["text"] # payload veri paketindeki verileri teker teker açarak text kısmındaki cevaba erişiyorum..!
+            #return result["candidates"][0]["content"]["parts"][0]["text"] # payload veri paketindeki verileri teker teker açarak text kısmındaki cevaba erişiyorum..!
+            return result["choices"][0]["message"]["content"]
         except Exception as e:
             return f"Yanıt hatasi {e}"
     else: # istek başarısızsa (hata varsa)
         return f"API hatası: {response.status_code}: {response.text}"
 
-def detect_intense(): # kullanıcıdan gelen mesajı niyet sınıflandırması ile sınıflandırmak
-    pass
+def detect_intent(user_message): # kullanıcıdan gelen mesajı niyet sınıflandırması ile sınıflandırıp ne istediğini anlamak..!
+    prompt = f"""
+        Kullanıcının aşağıdaki cümlesini sınıflandır:
+        Etiketlerden sadece birini döndür:
+        - not_ozet (eğer notlarla ilgili bir soru sorduysa yada not özetletmek istiyorsa)
+        - etkinlik_ozet (eğer etkinliklerle ilgili bir soru sorduysa yada etkinlik özetletmek istiyorsa)
+        - ozelgun_ozet (eğer özel günlerle ilgili bir soru yada özel günleri özetlemek istiyorsa)
+        - normal (diğer her şey)
+        
+        Cümle: {user_message}
+        Sadece etiket döndür: (örnek: not_ozet)
+        """
+    response = get_gemini_response(prompt)
+    return response.strip().lower()
 
 if __name__ == "__main__":
     user_input = input("Kullanıcı Sorusu: ") # terminal üzerinden girdi almak
