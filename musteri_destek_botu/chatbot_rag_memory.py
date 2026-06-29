1888d3819664b9de8ef6c2d309a612bf426e9349
Bora Eren Erdem <erdemeren3460@gmail.com>
2026-06-29 16:17:31 +0300

    Combining the RAG + memory + LLM model using a chain and testing the prototype in the terminal.

--- musteri_destek_botu/chatbot_rag_memory.py
+++ musteri_destek_botu/chatbot_rag_memory.py
@@ -37,3 +37,35 @@ Kütüphaneler:
     - pip install pypdf
     - pip install ollama
 """
+
+
+
+from langchain_ollama.chat_models import ChatOllama # ollama + gemma4:e4b
+from langchain_classic.chains import ConversationalRetrievalChain
+from langchain_classic.vectorstores import FAISS # vector_db
+from langchain_classic.memory import ConversationSummaryBufferMemory # memory
+from langchain_classic.embeddings import HuggingFaceEmbeddings # LaBSE embedding modeli
+
+
+llm = ChatOllama(model="gemma4:e4b", temperature=0.2) # ollama üzerinden kullanılacak on-prem llm modeli
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/LaBSE") # LaBSE embedding modeli
+
+vector_db = FAISS.load_local("faq_vector_store", embeddings, allow_dangerous_deserialization=True) # önceden oluşturulmuş vector_db'yi yükle
+# allow_dangerous_deserialization=True -> .pkl formatında dosya yüklerken hata oluşmasını önler.
+
+memory = ConversationSummaryBufferMemory(llm=llm, memory_key="chat_history", return_messages=True) # memory_key="chat_history" -> memory'nin hangi isimle saklanacağını belirtir. ConversationalRetrievalChain fonksiyonu memory'i ararken "chat_history" anahtar kelimesini kullanır..! RAG + memory yapılırken memory_key="chat_history" olarak belirtilir..!
+
+qa_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_db.as_retriever(search_kwargs={"k":3}), memory=memory, verbose=True)
+# ConversationalRetrievalChain.from_llm() -> RAG + memory + llm'i tek zincirde birleştirir.
+# retriever=vector_db.as_retriever(search_kwargs={"k":3}) -> vector_db'den getirici nesnesi getirir. as_retriever() ile vector_db'de arama yapar. search_kwargs={"k":3} ifadesi her arama yapıldığında en yakın 3 chunk'ı getirir..!
+
+# test
+print("müşteri destek botuna hoşgeldiniz.")
+while True:
+    user_input = input("kullanıcı: ")
+    if user_input.lower() in ["q", "quit", "exit"]:
+        print(f"byebyeee.")
+        break
+    response = qa_chain.invoke({"question": user_input})
+    print(f"müşteri destek botu: {response["answer"]}")
